0fb8c484e975438bf1f4aa56e9c99471d3d4dea5
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-08-02 19:37:09 +0100

    bug issue

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
+""" Index file for the api"""
 from flask import jsonify
 from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
+    """
+    Returns a JSON response with the status of the
+    API. The response contains a single key-value pair
+    Parameters:
+        None
+
+    Returns:
+        A JSON response with the status of the API.
+    """
     return jsonify({"status": "OK"})
