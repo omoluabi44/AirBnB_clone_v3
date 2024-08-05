feee7968f2a9869a45887fab01567ac6bfdbdddc
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-08-05 22:14:43 +0100

    finallY

--- apiss/v1/app.py
+++ apiss/v1/app.py
@@ -1,3 +1,32 @@
+# #!/usr/bin/python3
+# """The Flask API """
+# from flask import Flask, jsonify
+# import os
+# from models import storage
+# from api.v1.views import app_views
+
+# app = Flask(__name__)
+# app.register_blueprint(app_views)
+
+
+# @app.teardown_appcontext
+# def close_db(error):
+#     """ Close Storage """
+#     storage.close()
+
+
+# @app.errorhandler(404)
+# def not_found(error):
+#     """ 404 Error """
+#     return jsonify({"error": "Not found"}), 404
+
+
+# if __name__ == "__main__":
+#     """ main"""
+#     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+#     port = os.getenv("HBNB_API_PORT", "5000")
+#     app.run(host=host, port=port, threaded=True)
+
 #!/usr/bin/python3
 """ The Flask App """
 from models import storage
